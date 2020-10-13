f2a18d6a0558dfb438ebdde4e334c7c35e811290
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-13 20:08:42 +0200

    header

--- src/header.js
+++ src/header.js
@@ -2,19 +2,25 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <div className="left-site">Jaworski Daniel</div>
-      <div className="nav-bar">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Resume</li>
-          <li>Portfolio</li>
-          <li>Contact Me</li>
-        </ul>
+    <header>
+      <div className="darker-video">
+        <div className="container">
+          <div className="upperSiteHeader">
+            <div className="left-site">Jaworski Daniel</div>
+            <div className="nav-bar">
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Resume</li>
+                <li>Portfolio</li>
+                <li>Contact Me</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 export default Header;
