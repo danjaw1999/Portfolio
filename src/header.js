b99b51d9a7ddc7d589750dbb3ea416922e8197c4
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-13 20:01:45 +0200

    Structure of portfolio

---
+++ src/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="left-site">Jaworski Daniel</div>
+      <div className="nav-bar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Resume</li>
+          <li>Portfolio</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
