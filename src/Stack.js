31206205f78bfff39a87fea4d8426e851019ecf5
Daniel Jaworski <daniel.jaworski1999@gmail.com>
2020-10-16 21:12:03 +0200

    stack mobile

--- src/Stack.js
+++ src/Stack.js
@@ -1,35 +1,87 @@
 import React from "react";
 import { FormattedMessage } from "react-intl";
+import html from "./assets/html.svg";
+import css from "./assets/css.svg";
+import git from "./assets/git.png";
+import js from "./assets/js.png";
+import node from "./assets/node.svg";
+import react from "./assets/react.png";
+import npm from "./assets/npm.png";
+import gulp from "./assets/gulp.png";
+import mongo from "./assets/mongo.png";
+import php from "./assets/php.png";
+import babel from "./assets/babel.png";
+import webpack from "./assets/webpack.png";
 
-const Stack = () => {
+const Stack = (props) => {
   return (
     <div className="stack">
       <div className="container">
-          <div className="textInStack">
-              <span><FormattedMessage id="header.message.stack.skills" /></span>
-              <h2><FormattedMessage id="header.message.stack.stack" /></h2>
-              <hr></hr>
+        <div className="textInStack">
+          <span className="mainTitle" style={{ color: props.color }}>
+            <FormattedMessage id="header.message.stack.skills" />
+          </span>
+          <h2 className="secondaryTitle">
+            <FormattedMessage id="header.message.stack.stack" />
+          </h2>
+          <hr style={{ borderColor: props.color }}></hr>
+        </div>
+        <div className="technologyStack">
+          <div className="leftStack">
+            <div className="skill">
+              <img src={html} />
+              <p>HTML</p>
+            </div>
+            <div className="skill">
+              <img src={css} />
+              <p>CSS</p>
+            </div>
+            <div className="skill">
+              <img src={mongo} />
+              <p>MONGO</p>
+            </div>
+            <div className="skill">
+              <img src={babel} />
+              <p>BABEL</p>
+            </div>
           </div>
-          <div className="technologyStack">
-            <div className="leftStack">
-                <div className="skill">HTML</div>
-                <div className="skill">CSS</div>
-                <div className="skill">SCSS</div>
-                <div className="skill">BABEL</div>
+          <div className="midStack">
+            <div className="skill">
+              <img src={js} />
+              <p>JAVASCRIPT</p>
             </div>
-            <div className="midStack">
-            <div className="skill">JAVASCRIPT</div>
-                <div className="skill">REACT</div>
-                <div className="skill">Node.js</div>
-                <div className="skill">NPM</div>
+            <div className="skill">
+              <img src={react} />
+              <p>REACT</p>
             </div>
-            <div className="rightStack">
-            <div className="skill">GULP</div>
-                <div className="skill">GIT</div>
-                <div className="skill">WEBPACK</div>
-                <div className="skill">PHP</div>
+            <div className="skill">
+              <img src={node} />
+              <p>Node.js</p>
+            </div>
+            <div className="skill">
+              <img src={npm} />
+              <p>NPM</p>
             </div>
           </div>
+          <div className="rightStack">
+            <div className="skill">
+              <img src={gulp} />
+              <p>GULP</p>
+            </div>
+            <div className="skill">
+              <img src={git} />
+              <p>GIT</p>
+            </div>
+            <div className="skill">
+              <img src={webpack} />
+              <p>WEBPACK</p>
+            </div>
+            <div className="skill">
+              <img src={php} />
+              <p>PHP</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
